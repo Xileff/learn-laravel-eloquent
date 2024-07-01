07758ec777eb40600038ae47476bd427fe22c195
Felix <felixsavero78@gmail.com>
2024-07-01 14:06:25 +0700

    feat: has one of many

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Product;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\ProductSeeder;
@@ -21,5 +22,21 @@ class ProductTest extends TestCase
         $category = $product->category;
         $this->assertNotNull($category);
         $this->assertEquals('FOOD', $category->id);
+    }
+
+    public function testHasOneOfMany()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::find('FOOD');
+        $this->assertNotNull($category);
+
+        $cheapestProduct = $category->cheapestProduct;
+        $this->assertNotNull($cheapestProduct);
+        $this->assertEquals('1', $cheapestProduct->id);
+
+        $mostExpensiveProduct = $category->mostExpensiveProduct;
+        $this->assertNotNull($mostExpensiveProduct);
+        $this->assertEquals('2', $mostExpensiveProduct->id);
     }
 }
